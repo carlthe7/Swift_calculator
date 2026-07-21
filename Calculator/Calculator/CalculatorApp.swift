7be77f674ce1afcc708c2d77e2f9aa7ac1ac9392
Kenny Huynh <hnkworkmail.511@gmail.com>
2026-07-21 12:00:21 +1000

    Create the base of the commission calculator!

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kenny on 21/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
